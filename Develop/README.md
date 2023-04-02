a03aabfa970288727354e27ed798c02048a57506
Austin Lewis <austinlewis165@gmail.com>
2023-04-01 21:25:09 -0400

    finished javascript to make a generator, and included a sample readme used in the submission video

---
+++ Develop/README.md
@@ -0,0 +1,33 @@
+# README Generator
+![Project License](https://img.shields.io/badge/license-MIT-orange.svg)
+
+## Description
+Makes readmes. This is a sample product
+
+## Table of Contents
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contributions](#Contributions)
+* [Tests](#Tests)
+
+* [License](#license)
+
+## Installation
+oiwjefiwef
+
+## Usage
+]wefowiefjwef
+
+## Contributions
+N/A
+
+## Tests
+N/A
+
+## License
+This project is licensed under the MIT license.
+
+## Questions
+[My Github Account](https://github.com/AustinL96)
+
+Questions about this project? Reach out to me at [austinlewis165@gmail.com](mailto:austinlewis165@gmail.com)
--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,70 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const path = require('path');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+console.log('Hello and welcome to my README generator. Please fill out each question prompt to fill out your README. For any that do not apply, please either select None or write N/A.')
+const questions = [
+    
+    {
+        name: 'title',
+        message: 'What is the name of this project?'
+    },
+    {
+        name: 'description',
+        message: 'Describe your project. What purpose does it serve?'
+    },
+    {
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        name: 'projectUsage',
+        message: 'Provide instructions on how to use your project'
+    },
+    {
+        name: 'license',
+        message: 'Please select a license to apply to your project.',
+        type: 'list',
+        choices: ['MIT', 'Apache_2.0', 'Boost_1.0', 'None']
+    },
+    {
+        name: 'contributions',
+        message: 'Please provide the GitHub username of any contributors to your project.'
+    },
+    {
+        name: 'testing',
+        message: 'Write tests for your application and provide examples on how to run them here.'
+    },
+    {
+        name: 'githubName',
+        message: 'What is your GitHub username, for those that wish to follow you?'
+    },
+    {
+        name: 'yourEmail',
+        message: 'What is your email address, should anyone have questions for you?'
+    }
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFile('./README.md', data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+    console.log("You have successfully created your README file!");
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function (input) {
+        // console.log(input)
+        writeToFile("README.md", generateMarkdown(input))
+    })
+}
 
 // Function call to initialize app
 init();
